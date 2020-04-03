19823749a026f29f5c1145970aaac6ec8c05e04d
zhoyuhaogit config --global user.email sr8.13@qq.com <sr8.13@qq.com>
2020-04-03 23:23:57 +0800

    竣工修改小时 提交

--- src/main/java/com/accp/vo/zyh/zyhService_vo.java
+++ src/main/java/com/accp/vo/zyh/zyhService_vo.java
@@ -7,12 +7,34 @@ import com.accp.pojo.Servicedetailed;
 
 public class zyhService_vo extends Service{
 			
-	
+			private String bzname;
+			
+			private Integer type;
+			
+			private Integer hmoney;
 			
 			private String statucname;
 			
 			private List<Servicedetailed> list;
 			
+			public String getBzname() {
+				return bzname;
+			}
+			public void setBzname(String bzname) {
+				this.bzname = bzname;
+			}
+			public Integer getType() {
+				return type;
+			}
+			public void setType(Integer type) {
+				this.type = type;
+			}
+			public Integer getHmoney() {
+				return hmoney;
+			}
+			public void setHmoney(Integer hmoney) {
+				this.hmoney = hmoney;
+			}
 			public String getStatucname() {
 				return statucname;
 			}
