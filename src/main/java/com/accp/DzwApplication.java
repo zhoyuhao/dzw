e11b2f1939454dcc102e4ea159a065dc8aea5eea
zhouyuhao <sr8.13@qq.com>
2021-08-26 09:06:51 +0800

    测试git连接

--- src/main/java/com/accp/DzwApplication.java
+++ src/main/java/com/accp/DzwApplication.java
@@ -3,9 +3,7 @@ package com.accp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 测试git
- */
+
 @SpringBootApplication
 public class DzwApplication {
 
